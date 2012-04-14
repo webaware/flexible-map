1a5b44f66f7a3b745b0a2e7682461488eff1bd7e
Ross McKay <rmckay@webaware.com.au>
2012-04-14 23:21:13 +0000

    tag 1.1.0

---
+++ i18n/fr.js
@@ -0,0 +1,13 @@
+// French
+// from Google Translate -- please help improve this translation
+
+if (typeof FlexibleMap != "undefined") {
+
+	FlexibleMap.prototype.i18n["fr"] = {
+		"Click for details" : "Cliquez pour plus de détails",
+		"Directions" : "Itinéraire",
+		"From" : "Lieu de départ",
+		"Get directions" : "Itinéraire"
+	};
+
+}
---
+++ i18n/ms.js
@@ -0,0 +1,13 @@
+// Malaysian
+// from Google Translate -- please help improve this translation
+
+if (typeof FlexibleMap != "undefined") {
+
+	FlexibleMap.prototype.i18n["mk"] = {
+		"Click for details" : "Klik untuk maklumat lanjut",
+		"Directions" : "arahan",
+		"From" : "Tempat berlepas",
+		"Get directions" : "Dapatkan arah"
+	};
+
+}
