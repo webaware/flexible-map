aae5b0329b360dcfa1a9c36d8967caf63a56a312
Ross McKay <rmckay@webaware.com.au>
2012-05-20 23:04:41 +0000

    tag 1.1.2

---
+++ i18n/ru.js
@@ -0,0 +1,13 @@
+// Russian
+// from Google Translate -- please help improve this translation
+
+if (typeof FlexibleMap != "undefined") {
+
+	FlexibleMap.prototype.i18n["ru"] = {
+		"Click for details" : "Нажмите для подробностей",
+		"Directions" : "Карта проезда",
+		"From" : "Место отправления",
+		"Get directions" : "Получить карту направления"
+	};
+
+}
