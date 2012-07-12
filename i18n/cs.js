c0a08089ace40b2d8371d1baccd3c3523117b0a4
Ross McKay <rmckay@webaware.com.au>
2012-07-12 09:20:14 +0000

    tag 1.3.0

---
+++ i18n/cs.js
@@ -0,0 +1,13 @@
+// Czech
+// from Google Translate -- please help improve this translation
+
+if (typeof FlexibleMap != "undefined") {
+
+	FlexibleMap.prototype.i18n["cs"] = {
+		"Click for details" : "Klikněte pro detail",
+		"Directions" : "Mapa směry",
+		"From" : "Místo odletu",
+		"Get directions" : "Získat mapu trasy"
+	};
+
+}
