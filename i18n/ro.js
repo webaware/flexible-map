b396f7172398ca4a5c42626144cabc3e5f05d88f
Ross McKay <rmckay@webaware.com.au>
2012-09-29 03:37:46 +0000

    tag 1.5.0

---
+++ i18n/ro.js
@@ -0,0 +1,13 @@
+// Romanian
+// from Google Translate -- please help improve this translation
+
+if (typeof FlexibleMap != "undefined") {
+
+	FlexibleMap.prototype.i18n["ro"] = {
+		"Click for details" : "Click pentru detalii",
+		"Directions" : "direcţii",
+		"From" : "Locul de plecare",
+		"Get directions" : "Obţineţi indicaţii"
+	};
+
+}
