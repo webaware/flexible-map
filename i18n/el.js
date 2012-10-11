4b2cd77beee580797ac2f27c2bfab5e32df7626c
Ross McKay <rmckay@webaware.com.au>
2012-10-11 22:11:55 +0000

    tag 1.5.2

---
+++ i18n/el.js
@@ -0,0 +1,13 @@
+// Greek
+// from Google Translate -- please help improve this translation
+
+if (typeof FlexibleMap != "undefined") {
+
+	FlexibleMap.prototype.i18n["el"] = {
+		"Click for details" : "Κάντε κλικ για λεπτομέρειες",
+		"Directions" : "κατευθύνσεις Χάρτης",
+		"From" : "Τόπος αναχώρησης",
+		"Get directions" : "Πάρτε κατευθύνσεις χάρτη"
+	};
+
+}
---
+++ i18n/es.js
@@ -0,0 +1,13 @@
+// Spanish
+// from Google Translate -- please help improve this translation
+
+if (typeof FlexibleMap != "undefined") {
+
+	FlexibleMap.prototype.i18n["es"] = {
+		"Click for details" : "Haga clic para más detalles",
+		"Directions" : "Instrucciones",
+		"From" : "Lugar de salida",
+		"Get directions" : "Obtener direcciones"
+	};
+
+}
