2f0bf927ee31e705c8373dff51f6b7a7beec90b9
Ross McKay <rmckay@webaware.com.au>
2016-06-27 13:20:49 +1000

    provide link to get API key in settings

--- views/settings-form.php
+++ views/settings-form.php
@@ -17,7 +17,9 @@ if (!defined('ABSPATH')) {
 			<tr valign="top">
 				<th scope="row"><?php echo esc_html_x('API key', 'settings', 'wp-flexible-map'); ?></th>
 				<td>
-					<input type="text" class="large-text" name="flexible_map[apiKey]" value="<?php echo esc_attr($options['apiKey']); ?>" />
+					<input type="text" class="regular-text" name="flexible_map[apiKey]" value="<?php echo esc_attr($options['apiKey']); ?>" />
+					<br />
+					<em><?php printf(wp_kses_data(_x('Get your API key from the <a target="_blank" href="%s">Google Maps API website</a>.', 'settings', 'wp-flexible-map')), 'https://developers.google.com/maps/documentation/javascript/'); ?></em>
 				</td>
 			</tr>
 
