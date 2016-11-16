2759350add7e2c6a63dc05182f3c5b3c856d5048
Ross McKay <rmckay@webaware.com.au>
2016-11-16 19:35:46 +1100

    simplify SCRIPT_DEBUG test

--- includes/class.FlxMapPlugin.php
+++ includes/class.FlxMapPlugin.php
@@ -96,8 +96,8 @@ class FlxMapPlugin {
 			wp_register_script('google-maps', $apiURL, false, null, true);
 		}
 
-		$min = defined('SCRIPT_DEBUG') && SCRIPT_DEBUG ? '' : '.min';
-		$ver = defined('SCRIPT_DEBUG') && SCRIPT_DEBUG ? time() : FLXMAP_PLUGIN_VERSION;
+		$min = SCRIPT_DEBUG ? '' : '.min';
+		$ver = SCRIPT_DEBUG ? time() : FLXMAP_PLUGIN_VERSION;
 		wp_register_script('flxmap', "{$this->urlBase}js/flexible-map$min.js", array('google-maps'), $ver, true);
 
 		// theme writers: you can remove this stylesheet by calling wp_dequeue_script('flxmap');
