b834300ad75f566de40e104d0eda977d8c61759c
Ross McKay <rmckay@webaware.com.au>
2018-06-07 09:23:16 +1000

    add attributes to settings field to avoid browser meddling

--- views/settings-form.php
+++ views/settings-form.php
@@ -17,7 +17,7 @@ if (!defined('ABSPATH')) {
 			<tr valign="top">
 				<th scope="row"><?php echo esc_html_x('JavaScript API key', 'settings', 'wp-flexible-map'); ?></th>
 				<td>
-					<input type="text" class="regular-text" name="flexible_map[apiKey]" value="<?php echo esc_attr($options['apiKey']); ?>" />
+					<input type="text" class="regular-text" name="flexible_map[apiKey]" value="<?php echo esc_attr($options['apiKey']); ?>" autocorrect="off" autocapitalize="off" spellcheck="false" />
 					<p><?php
 						/* translators: {{a}} and {{/a}} will be replaced by HTML tags for a website link */
 						echo $this->addExternalLink(esc_html_x('Get your API key from the {{a}}Google Maps API website{{/a}}.', 'settings', 'wp-flexible-map'), 'https://cloud.google.com/maps-platform/#get-started');
@@ -28,7 +28,7 @@ if (!defined('ABSPATH')) {
 			<tr valign="top">
 				<th scope="row"><?php echo esc_html_x('Server API key', 'settings', 'wp-flexible-map'); ?></th>
 				<td>
-					<input type="text" class="regular-text" name="flexible_map[apiServerKey]" value="<?php echo esc_attr($options['apiServerKey']); ?>" />
+					<input type="text" class="regular-text" name="flexible_map[apiServerKey]" value="<?php echo esc_attr($options['apiServerKey']); ?>" autocorrect="off" autocapitalize="off" spellcheck="false" />
 					<p><?php echo esc_html_x('This key helps reduce the number of queries your server makes for maps with an address.', 'settings', 'wp-flexible-map'); ?></p>
 					<p><?php echo esc_html_x('It can be the same as the JavaScript key, but it must not be restricted by HTTP referrer. If you have restricted your JavaScript key by referrer, please create another key for the server.', 'settings', 'wp-flexible-map'); ?></p>
 				</td>
